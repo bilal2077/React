5435bd60b74b44eddd36231f387575b24f7e1e81
bilal <142602476+bilal2077@users.noreply.github.com>
2024-01-24 00:42:15 +0500

    Update Arrow.js

---
+++ Arrow.js
@@ -0,0 +1,50 @@
+// let ID ={
+//     username:"biala",
+//     age :19,
+
+//    welcomeMessage: function(){
+
+//         console.log(`hello ${this.username} Welcome to wabSaite `);
+//         console.log(this);
+//     }
+
+// }
+
+// ID.welcomeMessage()
+// ID.username = "usman"
+// ID.welcomeMessage()
+
+// console.log(this);
+
+
+//++++++++++++++++++++++++++++++++++++++++++ call <This> +++++++++++++
+
+// function chai() {
+//     console.log(this);
+// }chai()
+
+
+// function chai() {
+//     let name = "usman"
+//     console.lo
+//     g(this.name);
+// }
+    
+    
+    
+    
+
+// let chai = (num1 , num2) => {
+    
+//     return num1 + num2
+// }
+// 
+
+// console.log(chai(8,7));
+
+
+
+
+
+// let chai = (num1 , num2) =>   num1 + num2
+// console.log(chai(8,7));
