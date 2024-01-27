21244aad1ab1850743daa3217c140b06362deb0a
bilal <142602476+bilal2077@users.noreply.github.com>
2024-01-28 04:23:37 +0500

    push

--- advance/iterations.js
+++ advance/iterations.js
@@ -39,11 +39,11 @@ for (let i = 1; i <= 10 ; i++) {
 // }
 
 
-let myArray = ["flash", "batman", "superman"]
+let myArray = ["bilal", "usman", "saad"]
 // console.log(myArray.length);
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
-    console.log(element);
+    // console.log(element);
     
 }
 
@@ -71,13 +71,104 @@ for (let index = 0; index < myArray.length; index++) {
 
 
 
-
-// let arr =["bilal","usman","saad"]
-   
-
 // let a = 0;
-
 // while (a < arr.length){
-//        console.log(`velue is ${arr[a]}`);
-//        a = a + 1
+      
+//       console.log(`velue is ${arr[a]}`);
+//        a = a + 1 
 // }
+
+// =======================DO WHIEL LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOP++++++++++++++++++++
+
+
+// let  = ["bilal", "usman", "saad"]
+
+// let score =  0;
+
+// do {
+//     console.log(myArray[score]);
+//       score++
+// }while (score <= myArray.length )
+
+
+
+//============================= FOR OF LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOP+++++++++++++++++++++
+
+let arrry = [1,2,3,4,5,6,]
+
+for (const num of arrry) {
+    
+    // console.log(num);
+
+}
+
+ let staring = "hello world!"
+for (const hello of staring) {
+    
+    // console.log(hello);
+    
+}
+// ===============OFR IN LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOP
+
+let obj  = {
+    js: `javaScript`,
+    TS: `TypeScript`,
+    CPP: "c++"
+}
+
+for (const name in obj) {
+    // console.log(`${name}  is ${obj[name]} `);
+}
+
+let naam = ["bilal","usamn","saad"]
+
+for (const key1 in naam) {
+   
+        // console.log(key1, " : " ,naam[key1]);
+    
+}
+
+
+// ============================ MAP .JS ================+++
+
+ const map = new Map();
+ map.set (`PK,`,"pakistan")
+ map.set (`IN,`,"india")
+ map.set (`US,`,"unaited state of Amarica")
+
+//  console.log(map);
+
+for (const [world, velue] of map) { //for of loop
+    // console.log(world,"  :", velue);
+}
+ 
+
+
+//=-====================FOR eacH loooooooooooooooooooooooooooooooop========================================
+
+let months = [" ","jun","fab","mar","aip","may","jun"]
+
+
+months.forEach((vulu,index)  =>  {
+    // console.log(vulu,index);
+    
+});
+
+
+
+
+let ao = [
+    { langugeaname:"javaScrip",
+      filename: "js" },
+
+    { langugeaname:"namejava",
+      filename: "java" },
+
+    { langugeaname:"python",
+      filename: "py" }
+]
+
+
+ao.forEach((val) => {
+    console.log(val.langugeaname);
+});
