f745f75cc14900c5966a4efc80100a15fbc3b5ad
Bilal khan <142602476+bilal2077@users.noreply.github.com>
2024-09-27 13:12:03 +0500

    p

--- 03tailwindprops/src/main.jsx
+++ 03tailwindprops/src/main.jsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
-
-function MyApp() {
-    return (
-      <>
-      <h1>hello Vite !</h1>
-      </>
-    )
-  }
-
-
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
-    MyApp() 
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
--- vite/src/chia.jsx
+++
@@ -1,7 +0,0 @@
-function Chai() {
-    return(
-        <h1>Chai page </h1>
-    )
-}
-
-export default Chai;
