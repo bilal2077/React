1a64da933726c8025f93f6e5e6b71c10c58224b0
Bilal khan <142602476+bilal2077@users.noreply.github.com>
2024-09-27 17:11:57 +0500

    p

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+       
       <h1 className='bg-green-400 text-black p-4 rounded-xl'>hello world</h1>
-
+       <Card username="usman" age="21"/>
+       <Card  username="bilal"/>
     </>
   )
 }
